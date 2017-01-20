31d5ac5527db425390e51c7681ab853cc9d41908
Gedeon Ntwali <ntwari28@gmail.com>
2017-01-20 15:13:40 +0200

    I used a form method to play a guess number game

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -12,5 +12,35 @@ class ParamsController < ApplicationController
     puts @number
     render "my_number.html.erb"
   end
+
+  def url_segment_params_url
+    @message = params[:message]
+    if @message.class == String
+    puts @message
+    render "url_segment_params_url.html.erb"
+    else
+    @message = "Enter...."
+    end
+  end
+
+  def form_game
+     render "form_game.html.erb"
+  end
+
+
+  def result_game
+    @guess = params[:form_number]
+    number = 45 
+    if number == @guess.to_i
+      @result = "That's the riht number, You Win!!!!!" 
+
+    elsif number < @guess.to_i 
+      @result = "Almost there but try to guess lower next time!!"
+  
+    else number > @guess.to_i
+      @result = "Nearly there but try to guess higher next time !!"
+    end
+    render "result_game.html.erb"
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/my_name", to:'params#my_name'
   get "/my_number", to:'params#your_number'
+  get "/url_segment_params_url/:message", to:'params#url_segment_params_url'
+  get "/form_game" , to:'params#form_game'
+  post "/result_game", to: 'params#result_game'
 end
